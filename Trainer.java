f346ab44604d7c0fce916baea27e5538e3611e0d
jimyuan42x <jimyuan42x@gmail.com>
2014-01-14 13:39:09 -0500

    Create Trainer.java
    generic trainers

---
+++ Trainer.java
@@ -0,0 +1,21 @@
+//File for the Trainer class
+//This encompasses the Player, the Gym Leaders, and the random trainers
+
+
+public class Trainer{
+    //variables for generic trainers:
+    int _moneys;
+    String _name;
+    String[] _pokemons;
+
+
+
+    public Trainer(String name){
+	_name = name;
+	_pokemons = new String[6];
+
+    }
+
+
+
+}
