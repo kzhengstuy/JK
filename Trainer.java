c823dc29dfa305e2749ec2731a08ead265dd97a9
jimyuan42x <jimyuan42x@gmail.com>
2014-01-14 13:56:07 -0500

    on-hand party is now an arraylist

--- Trainer.java
+++ Trainer.java
@@ -1,18 +1,19 @@
 //File for the Trainer class
 //This encompasses the Player, the Gym Leaders, and the random trainers
 
+import java.util.ArrayList;
 
 public class Trainer{
     //variables for generic trainers:
     int _moneys;
     String _name;
-    String[] _pokemons;
+    ArrayList<String> _pokemons;
 
 
 
     public Trainer(String name){
 	_name = name;
-	_pokemons = new String[6];
+	
 
     }
 
