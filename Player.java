ca78bb43025f562bf4e19138b2e6de3367a1e615
jimyuan42x <jimyuan42x@gmail.com>
2014-01-14 13:42:31 -0500

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,23 @@
+//file for the Player class
+//this guy is the main character, the one the player plays as
+
+
+public class Player extends Trainer{
+    int _numBadges;
+
+
+
+    public Player(String name){
+	super(name);
+	_numBadges = 0;
+	_moneys = 2000;
+
+
+    }
+
+
+
+    //will have to make player a method for picking the first pokemon
+
+
+}
