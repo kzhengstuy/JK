2d4ce96137c7546e6b6bd38cd46bdaf6c1b38159
jimyuan42x <jimyuan42x@gmail.com>
2014-01-14 13:57:34 -0500

    Create GymLeader.java
    gym leaders. really generic.

---
+++ GymLeader.java
@@ -0,0 +1,17 @@
+/file for the Gym Leaders
+//basically, brock, misty, etc.
+
+
+
+public class GymLeader extends Trainer{
+
+
+    int _winMoney;
+
+    public GymLeader(String name){
+
+	super(name);
+    }
+
+
+}
