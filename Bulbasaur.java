e02cb33885dfcbe28345a14f4f2f24298dcb2508
kzhengstuy <kzheng5tt@gmail.com>
2014-01-15 13:59:07 -0500

    works!!

--- Bulbasaur.java
+++ Bulbasaur.java
@@ -1,6 +1,22 @@
 public class Bulbasaur extends Pokemon {
-    Pokemon Bulbasuar = new Pokemon();
- 
+    
+
+
+    public Bulbasaur(){
+
+	super("Bulbasaur");
+	_type = "Grass/Poison";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+	Bulbasaur pkn = new Bulbasaur();
+	System.out.println(pkn._level);
+	
+    }
+    
 
    
 }
