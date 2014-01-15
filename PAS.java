f5fc093b3d16d9ddf6215b611d485500743f4b74
jimyuan42x <jimyuan42x@gmail.com>
2014-01-15 13:50:48 -0500

    Create PAS.java

---
+++ PAS.java
@@ -0,0 +1,20 @@
+//driver file meant to run the pokemon adventure simulator
+
+
+
+import cs1.Keyboard;
+public class PAS{
+
+    public static void main(String[]args){
+
+
+
+
+    }
+
+
+
+
+
+
+}
