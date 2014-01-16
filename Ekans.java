ad5ed682e796a34460c47c8c6819a642507f5546
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:50:31 -0500

    Create Ekans.java

---
+++ Ekans.java
@@ -0,0 +1,22 @@
+public class Bulbasaur extends Pokemon {
+    
+
+
+    public Bulbasaur(){
+
+        super("Bulbasaur");
+        _type = "Grass/Poison";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Bulbasaur pkn = new Bulbasaur();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
