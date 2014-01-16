9956d271df109fb96ab6d4e1aaefe667bc6dc8f8
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:55:10 -0500

    Create Clefairy.java

---
+++ Clefairy.java
@@ -0,0 +1,22 @@
+public class Clefairy extends Pokemon {
+    
+
+
+    public Clefairy(){
+
+        super("Clefairy");
+        _type = "Normal";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Clefairy pkn = new Clefairy();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
