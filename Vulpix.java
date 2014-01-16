63bdb8a517cb6e97ad7bf687161a90f71c28abc5
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:55:38 -0500

    Create Vulpix.java

---
+++ Vulpix.java
@@ -0,0 +1,22 @@
+public class Vulpix extends Pokemon {
+    
+
+
+    public Vulpix(){
+
+        super("Vulpix");
+        _type = "Fire";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Vulpix pkn = new Vulpix();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
