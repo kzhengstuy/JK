177ffaed06e041df7786b0e9119c4c6a456be25f
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:40:14 -0500

    Create Caterpie.java

---
+++ Caterpie.java
@@ -0,0 +1,22 @@
+public class Caterpie extends Pokemon {
+    
+
+
+    public Caterpie(){
+
+        super("Caterpie");
+        _type = "Bug";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Caterpie pkn = new Caterpie();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
