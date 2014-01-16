6cd6ada43cafa67344302920ed2f15ff7321161b
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:52:05 -0500

    Create Pikachu.java

---
+++ Pikachu.java
@@ -0,0 +1,23 @@
+public class Pikachu extends Pokemon {
+    
+
+
+    public Pikachu(){
+
+        super("Pikachu");
+        _type = "Electric";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Pikachu pkn = new Pikachu();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
+    
