91b9b13c8c67e2b3280b7252daab572bc98e59ba
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:36:16 -0500

    Create Squirtle.java

---
+++ Squirtle.java
@@ -0,0 +1,22 @@
+public class Squirtle extends Pokemon {
+    
+
+
+    public Squirtle(){
+
+        super("Squirtle");
+        _type = "Water";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Squirtle pkn = new Squirtle();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
