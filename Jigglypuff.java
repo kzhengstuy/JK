64b1c21ecf44b3fff385dbed79b087a9dd4624a9
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:56:48 -0500

    Create Jigglypuff.java

---
+++ Jigglypuff.java
@@ -0,0 +1,22 @@
+public class Jigglypuff extends Pokemon {
+    
+
+
+    public Jigglypuff(){
+
+        super("Jigglypuff");
+        _type = "Normal";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Jigglypuff pkn = new Jigglypuff();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
