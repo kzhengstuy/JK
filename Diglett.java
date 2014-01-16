b8475933d6dfb5da58f3c5a7f3593ec2fdb4e946
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:59:55 -0500

    Create Diglett.java

---
+++ Diglett.java
@@ -0,0 +1,22 @@
+public class Diglett extends Pokemon {
+    
+
+
+    public Diglett(){
+
+        super("Diglett");
+        _type = "Ground";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Diglett pkn = new Diglett();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
