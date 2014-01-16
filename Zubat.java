f75b9bde5a4d8242ae0bcc043ffc737f51c60b97
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:57:17 -0500

    Create Zubat.java

---
+++ Zubat.java
@@ -0,0 +1,22 @@
+public class Zubat extends Pokemon {
+    
+
+
+    public Zubat(){
+
+        super("Zubat");
+        _type = "Poison/Flying";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Zubat pkn = new Zubat();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
