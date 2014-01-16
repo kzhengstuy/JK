efcfc1844d85b1b6567f21a7cde0b41999acf0b1
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:58:01 -0500

    Create Oddish.java

---
+++ Oddish.java
@@ -0,0 +1,22 @@
+public class Oddish extends Pokemon {
+    
+
+
+    public Oddish(){
+
+        super("Oddish");
+        _type = "Grass/Poison";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Oddish pkn = new Oddish();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
