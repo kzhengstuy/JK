65eb05fc5e84cec3e88bf018bcbc47d5f11792f3
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:58:41 -0500

    Create Paras.java

---
+++ Paras.java
@@ -0,0 +1,22 @@
+public class Paras extends Pokemon {
+    
+
+
+    public Paras(){
+
+        super("Paras");
+        _type = "Bug/Grass";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Paras pkn = new Paras();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
