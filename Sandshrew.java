64de2faf9a814e2f1f916068bac659b2f1d7a8f5
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:52:58 -0500

    Create Sandshrew.java

---
+++ Sandshrew.java
@@ -0,0 +1,23 @@
+public class Sandshrew extends Pokemon {
+    
+
+
+    public Sandshrew(){
+
+        super("Sandshrew");
+        _type = "Ground";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Sandshrew pkn = new Sandshrew();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
+    
