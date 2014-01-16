8c159b18e235d99cad781a7ec95d90fb6c48142e
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:45:10 -0500

    Create Rattata.java

---
+++ Rattata.java
@@ -0,0 +1,22 @@
+public class Rattata extends Pokemon {
+    
+
+
+    public Rattata(){
+
+        super("Rattata");
+        _type = "Normal";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Rattata pkn = new Rattata();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
