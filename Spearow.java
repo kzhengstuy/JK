144eecdf31e9c38eb6ca5f1e13edb16b721421bb
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:47:23 -0500

    Create Spearow.java

---
+++ Spearow.java
@@ -0,0 +1,22 @@
+public class Spearow extends Pokemon {
+    
+
+
+    public Spearow(){
+
+        super("Spearow");
+        _type = "Normal";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Spearow pkn = new Spearow();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
