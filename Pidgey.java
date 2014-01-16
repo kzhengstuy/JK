4422f9e95f98d8174600f5d57f5964e7200706a2
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:38:41 -0500

    Create Pidgey.java

---
+++ Pidgey.java
@@ -0,0 +1,22 @@
+public class Pidgey extends Pokemon {
+    
+
+
+    public Pidgey(){
+
+        super("Pidgey");
+        _type = "Normal/Flying";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Pidgey pkn = new Pidgey();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
