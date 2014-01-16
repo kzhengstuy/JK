147e9a464c5766b11e429c92fb46fad4876cfafc
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:43:04 -0500

    Create Weedle.java

---
+++ Weedle.java
@@ -0,0 +1,22 @@
+public class Weedle extends Pokemon {
+    
+
+
+    public Weedle(){
+
+        super("Weedle");
+        _type = "Bug/Poison";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Weedle pkn = new Weedle();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
