21d3f2193aba088f80d99f7ea7f209afcfd1c691
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:54:36 -0500

    Create NidoranM.java

---
+++ NidoranM.java
@@ -0,0 +1,22 @@
+public class NidoranM extends Pokemon {
+    
+
+
+    public NidoranM(){
+
+        super("NidoranM");
+        _type = "Poison";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        NidoranM pkn = new NidoranM();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
