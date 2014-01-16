04e35febc512637e6f4d6e331b5e0e3af71b1df7
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 13:59:26 -0500

    Create Venonat.java

---
+++ Venonat.java
@@ -0,0 +1,22 @@
+public class Venonat extends Pokemon {
+    
+
+
+    public Venonat(){
+
+        super("Venonat");
+        _type = "Bug/Poison";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Venonat pkn = new Venonat();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
