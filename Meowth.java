f926bfff2ab3cdad2291174d91afa2281492d698
kzhengstuy <kzheng5tt@gmail.com>
2014-01-16 14:00:33 -0500

    Create Meowth.java

---
+++ Meowth.java
@@ -0,0 +1,22 @@
+public class Meowth extends Pokemon {
+    
+
+
+    public Meowth(){
+
+        super("Meowth");
+        _type = "Normal";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Meowth pkn = new Meowth();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
