a6d8a1db25b62adec6c05733248322b9a6524717
kzhengstuy <kzheng5tt@gmail.com>
2014-01-17 02:46:32 -0500

    Create Magnemite.java

---
+++ Magnemite.java
@@ -0,0 +1,22 @@
+public class Magnemite extends Pokemon {
+    
+
+
+    public Magnemite(){
+
+        super("Magnemite");
+        _type = "Electric/Steel";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Magnemite pkn = new Magnemite();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
