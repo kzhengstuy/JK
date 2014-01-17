2957f6d77b5c0575b4a9e1877123bfb0474ea668
kzhengstuy <kzheng5tt@gmail.com>
2014-01-17 02:32:37 -0500

    Create Machop.java

---
+++ Machop.java
@@ -0,0 +1,22 @@
+public class Machop extends Pokemon {
+    
+
+
+    public Machop(){
+
+        super("Machop");
+        _type = "Fighting";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Machop pkn = new Machop();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
