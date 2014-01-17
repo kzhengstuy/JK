0c4f653609647c7433fd78904dad26b974c4a7c5
kzhengstuy <kzheng5tt@gmail.com>
2014-01-17 02:34:19 -0500

    Create Geodude.java

---
+++ Geodude.java
@@ -0,0 +1,22 @@
+public class Geodude extends Pokemon {
+    
+
+
+    public Geodude(){
+
+        super("Geodude");
+        _type = "Rock/Ground";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Geodude pkn = new Geodude();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
