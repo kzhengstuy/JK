cb2485af9c1e8be1116cec136d2c297d337a5ab6
kzhengstuy <kzheng5tt@gmail.com>
2014-01-17 02:25:18 -0500

    Create Psyduck.java

---
+++ Psyduck.java
@@ -0,0 +1,23 @@
+public class Psyduck extends Pokemon {
+    
+
+
+    public Psyduck(){
+
+        super("Psyduck");
+        _type = "Water";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Psyduck pkn = new Psyduck();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
+    
