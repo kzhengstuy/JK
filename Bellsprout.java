67d433787d38fefdbc5d40f7bf0aba89db61a5b8
kzhengstuy <kzheng5tt@gmail.com>
2014-01-17 02:33:10 -0500

    Create Bellsprout.java

---
+++ Bellsprout.java
@@ -0,0 +1,22 @@
+public class Bellsprout extends Pokemon {
+    
+
+
+    public Bellsprout(){
+
+        super("Bellsprout");
+        _type = "Grass/Poison";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Bellsprout pkn = new Bellsprout();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
