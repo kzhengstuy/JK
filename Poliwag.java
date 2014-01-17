fe44a1e5167873ec2362b36229e8b13cb53f8294
kzhengstuy <kzheng5tt@gmail.com>
2014-01-17 02:29:11 -0500

    Create Poliwag.java

---
+++ Poliwag.java
@@ -0,0 +1,22 @@
+public class Poliwag extends Pokemon {
+    
+
+
+    public Poliwag(){
+
+        super("Poliwag");
+        _type = "Water";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Poliwag pkn = new Poliwag();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
