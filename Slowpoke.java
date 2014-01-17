17fc828a59f81cd87d5f41cc7aa5d2ad54fd3fc9
kzhengstuy <kzheng5tt@gmail.com>
2014-01-17 02:44:57 -0500

    Create Slowpoke.java

---
+++ Slowpoke.java
@@ -0,0 +1,22 @@
+public class Slowpoke extends Pokemon {
+    
+
+
+    public Slowpoke(){
+
+        super("Slowpoke");
+        _type = "Fire";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Slowpoke pkn = new Slowpoke();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
