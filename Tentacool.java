5d4f95cb828a8b562e88f0a41a35a8eb495b96c9
kzhengstuy <kzheng5tt@gmail.com>
2014-01-17 02:33:45 -0500

    Create Tentacool.java

---
+++ Tentacool.java
@@ -0,0 +1,22 @@
+public class Tentacool extends Pokemon {
+    
+
+
+    public Tentacool(){
+
+        super("Tentacool");
+        _type = "Water/Poison";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Tentacool pkn = new Tentacool();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
