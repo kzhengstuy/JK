e72ad3efad9a2b013d1f1af258618910af861a68
kzhengstuy <kzheng5tt@gmail.com>
2014-01-17 02:25:52 -0500

    Create Mankey.java

---
+++ Mankey.java
@@ -0,0 +1,23 @@
+public class Mankey extends Pokemon {
+    
+
+
+    public Mankey(){
+
+        super("Psyduck");
+        _type = "Fighting";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Mankey pkn = new Mankey();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
+    
