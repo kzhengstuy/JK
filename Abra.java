3ae1ba4d9f38766fe196a03596391a8fb3a308f5
kzhengstuy <kzheng5tt@gmail.com>
2014-01-17 02:29:44 -0500

    Create Abra.java

---
+++ Abra.java
@@ -0,0 +1,22 @@
+public class Abra extends Pokemon {
+    
+
+
+    public Abra(){
+
+        super("Abra");
+        _type = "Psychic";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Abra pkn = new Abra();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
