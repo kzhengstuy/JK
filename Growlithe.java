cc56c872010d82973f31495ed011949610364e01
kzhengstuy <kzheng5tt@gmail.com>
2014-01-17 02:26:18 -0500

    Create Growlithe.java

---
+++ Growlithe.java
@@ -0,0 +1,22 @@
+public class Growlithe extends Pokemon {
+    
+
+
+    public Growlithe(){
+
+        super("Growlithe");
+        _type = "Fire";
+
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Growlithe pkn = new Growlithe();
+        System.out.println(pkn._level);
+        
+    }
+    
+
+   
+}
