a07d6b0c66cc51ca4915a720b0cff7fe663822a2
jimyuan42x <jimyuan42x@gmail.com>
2014-01-19 12:38:26 -0500

    Player can't really pick a first Pokemon
    In PAS and Player are the issues. The addPokemon(Pokemon pkn) is the
    problem.

--- PAS.java
+++ PAS.java
@@ -3,6 +3,8 @@
 
 
 import cs1.Keyboard;
+
+import java.util.ArrayList;
 public class PAS{
     //generic player
     static Player player;
@@ -12,36 +14,52 @@ public class PAS{
 
 
 	System.out.println("Right... so your name is " + player._name + "? Are you sure? [Y/N]");
-	if (Keyboard.readString().equals("Y")){
-	    play();
-	    }
-	else if(Keyboard.readString().equals("N")){
+	String string = Keyboard.readString();
+	if(string.equals("N")){
 	    System.out.println("Okay... so what's your name?");
 	    player._name = Keyboard.readString();
 	    beginning();
 	}
+	else if (string.equals("Y")){
+	    play();
+	}
 
     }
 
+    public static void pick(){//picking the first pokemon
+	System.out.println("You have three choices to pick from:\n1)Bulbasaur\t2)Charmander\t3)Squirtle\nDon't be shy! Pick one! [1/2/3]");
+	String choice = Keyboard.readString();
+	if( choice.equals("1")){
+	    player.addPokemon(new Bulbasaur());
+	    }
+	System.out.println("So this is okay?");
+	System.out.println(player.getPokemons());
 
+    }
 
 	public static void play(){
 	    System.out.println("Time to play!!\nWe'll start by picking your first Pokemon: ");
+	    pick();
 	    
 	}
 
 
 	public static void main(String[]args){
 	
-	
-	System.out.println("Okay, this is Pokemon. Not much explainging or story to do. Let's get started. What's your name?");
+	    /*    player = new Player("Jim");
+	    Bulbasaur pkmn = new Bulbasaur();
+	    player.addPokemon(pkmn);
+	    System.out.println(player.getPokemons());
+	    */
+
+	    System.out.println("Okay, this is Pokemon. Not much explainging or story to do. Let's get started. What's your name?");
 	
 	player = new Player(Keyboard.readString());
 	
 	beginning();
 	
 	
-	
+	    
 	}
 	
 	
--- Player.java
+++ Player.java
@@ -2,6 +2,8 @@
 //this guy is the main character, the one the player plays as
 
 
+import java.util.ArrayList;
+
 public class Player extends Trainer{
     int _numBadges;
 
@@ -30,6 +32,35 @@ public class Player extends Trainer{
 
     public double getMoney(){
 	return _moneys;
+    }
+
+
+    public  ArrayList<Pokemon> getPokemons(){
+	return _pokemons;
+    }
+
+
+    public Pokemon addPokemon(Pokemon pkn){
+	
+	getPokemons().add(pkn);
+	return pkn;
+    }
+    public String toString(){
+
+	String ret = "";
+	ret += "Trainer: " + getName();
+	ret += getPokemons();
+
+	return ret;
+    }
+
+
+
+    public static void main(String[] args){
+	Player bob = new Player("Bob");
+	Pokemon poke = new Bulbasaur();
+	bob.addPokemon(poke);
+
     }
 
 }
