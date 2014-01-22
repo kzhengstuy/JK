4431db4da6ad02d3c102f68d2346f689bf06a45e
kzhengstuy <kzheng5tt@gmail.com>
2014-01-22 01:17:39 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,19 +1,26 @@
-//file for the Player class
+	//file for the Player class
 //this guy is the main character, the one the player plays as
 
 
 import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+import cs1.Keyboard;
 
 public class Player extends Trainer{
     int _numBadges;
-
-
+    private InputStreamReader isr;
+    private BufferedReader in;
+ 
+    
 
     public Player(String name){
 	super(name);
 	_numBadges = 0;
 	_moneys = 2000;
-
+	isr = new InputStreamReader( System.in );
+	in = new BufferedReader( isr );
+    
 
     }
 
@@ -42,7 +49,7 @@ public class Player extends Trainer{
 
     public Pokemon addPokemon(Pokemon pkn){
 	
-	getPokemons().add(pkn);
+	_pokemons.add(pkn);
 	return pkn;
     }
     public String toString(){
@@ -58,28 +65,33 @@ public class Player extends Trainer{
 
     public static void main(String[] args){
 	Player bob = new Player("Bob");
+	int i = 1;
+	
+	try {
+	    System.out.println( "Pick your Pokemon!" );
+	    System.out.println( "\t1: Bulbasaur \n\t2: Charmander\n\t3: Squirtle" );
+	    i = Integer.parseInt( in.readLine() );
+	}	
+	catch ( IOException e ) { }
     
-    try {
-    	System.out.println( "Pick your Pokemon!" );
-    	System.out.println( "\t1: Bulbasaur \n\t2: Charmander \n\t3: Squirtle" );
-    	i = Integer.parseInt( in.readLine() );
-    }	
-    catch ( IOException e ) { }
+	if ( i == 1 ) {
+	    Bulbasaur b = new Bulbasaur();
+	    bob.addPokemon(b);
+	}
     
-    if ( i == 1 ) {
-    	addPokemon("Bulbasaur")
-    }
+	else if (i == 2) {
+	    Charmander c = new Charmander();
+	    bob.addPokemon(c);
+	}
     
-    else if (i == 2) {
-    	addPokemon("Charmander")
-    }
-    
-    else { 
-    	addPokemon("Squirtle")
+	else { 
+	    Squirtle s = new Squirtle();
+	    bob.addPokemon(s);
     	
+	}
+    
     }
-    	
     
     
-
 }
+
