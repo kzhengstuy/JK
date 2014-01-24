78af212f6e569f9134e6fb0a0f2ff36c68cf6f38
kzhengstuy <kzheng5tt@gmail.com>
2014-01-24 02:40:18 -0500

    ...

--- PAS.java
+++ PAS.java
@@ -6,136 +6,102 @@ import cs1.Keyboard;
 import java.io.*;
 import java.util.*;
 import java.util.ArrayList;
+
 public class PAS{
     //generic player
     static Player player;
     private InputStreamReader isr;
     private BufferedReader in;
-
+    ArrayList _pokemons = new ArrayList();
 
     public static void beginning(){
 
 
-	System.out.println("Right... so your name is " + player._name + "? Are you sure? [Y/N]");
-	String string = Keyboard.readString();
-	if(string.equals("N")){
-	    System.out.println("Okay... so what's your name?");
-	    player._name = Keyboard.readString();
-	    beginning();
-	}
-	else if (string.equals("Y")){
-	    play();
-	}
+        System.out.println("Right... so your name is " + player._name + "? Are you sure? [Y/N]");
+        String string = Keyboard.readString();
+        if(string.equals("N")){
+            System.out.println("Okay... so what's your name?");
+            player._name = Keyboard.readString();
+            beginning();
+        }
+        else if (string.equals("Y")){
+            play();
+        }
 
     }
+   
+    public static ArrayList<Pokemon> getPokemons(){
+        return player._pokemons;
+    }
+
     public static void addPokemon(Pokemon pkn){// adding a pokemon to the party
+        player.getPokemons().add(pkn);
 	
-	player._pokemons.add(pkn);
-	
+        
     }
 
 
-    public  static ArrayList<Pokemon> getPokemons(){
-	return player._pokemons;
-    }
+    /* public static ArrayList<Pokemon> getPokemons(){
+        return player._pokemons;
+	}*/
 
     public static void pick(){//picking the first pokemon
-	System.out.println("You have three choices to pick from:\n1)Bulbasaur\t2)Charmander\t3)Squirtle\nDon't be shy! Pick one! [1/2/3]");
-	String choice = Keyboard.readString();
+        System.out.println("You have three choices to pick from:\n1)Bulbasaur\t2)Charmander\t3)Squirtle\nDon't be shy! Pick one! [1/2/3]");
+        String choice = Keyboard.readString();
 	Pokemon select;
-	if( choice.equals(1)){
+
+        if( choice.equals(1)){
 	    select = new Bulbasaur();
-	    addPokemon(select);
+	    player.addPokemon(select);
 	    // System.out.println(select);
-	}
-		else if (choice.equals(2)){
-	    select = new Bulbasaur();
-	    addPokemon(select);
-	    // System.out.print(select);
-	}
-	else if (choice.equals(3)){
-	    select = new Bulbasaur();
-	    addPokemon(select);
-	    // System.out.print(select);
-	}
-	System.out.println("So this is okay?[Y/N]");
-
-	System.out.println(getPokemons()); 
-
-	if (Keyboard.readString().equals("N")){
-	    pick();
-	}
-	
-    }
-
-	public static void play(){
-	    System.out.println("Time to play!!\nWe'll start by picking your first Pokemon: ");
-	    pick();
-	    
-	}
-
-
-    public static void heal(){
-	
-	    for(Pokemon p: player._pokemons){
-		p._hp = p._maxHP;
-	
-	    }
-	    System.out.println("Your Pokemon have all been healed!");
-	
-	
-    }
-
-
-
-
-    public static void battle(){
-
-
-
+        }
+	else if (choice.equals(2)){
+	    select = new Charmander();
+	    player.addPokemon(select);
+            // System.out.print(select);
+        }
+        else if (choice.equals(3)){
+	    select = new Squirtle();
+	    player.addPokemon(select);
+            // System.out.print(select);
+        }
+        
+	System.out.println("Let's go into battle!");
+	playTurn();
 
     }
 
-
-    public static void run(){
-	while(true){
-	    System.out.println("1)Heal Pokemon\t2)Battle Trainers");
-	    String choice = Keyboard.readString();
-	    if (choice.equals(1)){
-		heal();
-	    }
-	    if (choice.equals(2)){
-		battle();
-	    }
-	}
-
-    }
-
-
-	public static void main(String[]args){
-	
-	    /*    player = new Player("Jim");
-	    Bulbasaur pkmn = new Bulbasaur();
-	    player.addPokemon(pkmn);
-	    System.out.println(player.getPokemons());
-	    */
-
-	    System.out.println("Okay, this is Pokemon. Not much explainging or story to do. Let's get started. What's your name?");
-	
-	player = new Player(Keyboard.readString());
-	
-	beginning();
-	
-	System.out.println("Welp, time to play, I guess. What would you like to do?");
-	
-	run();
-	    
-	}
-	
-	
-	
-	
-	
+    public static boolean playTurn() {
+	int i = 1;
+	int d1, d2;
+	System.out.println("Oh look, an enemy!");
+	return true;
 	
     }
+
+        public static void play(){
+            System.out.println("Time to play!!\nWe'll start by picking your first Pokemon: ");
+            pick();
+            
+        }
+
+
+        public static void main(String[]args){
+        
+          
+            System.out.println("Okay, this is Pokemon. Not much explainging or story to do. Let's get started. What's your name?");
+        
+        player = new Player(Keyboard.readString());
+        
+        beginning();
+        
+        
+            
+        }
+        
+        
+        
+        
+}
+
     
